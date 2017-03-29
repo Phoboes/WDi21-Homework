090e8f11d8e7f08890638e6dd0aac3bf2e2a6df5
fimac <fimccawley@gmail.com>
2017-03-29 19:28:57 +1100

    updated functions homework

--- fiona_mccawley/week_01/day_03/functions_hw/main.js
+++ fiona_mccawley/week_01/day_03/functions_hw/main.js
@@ -2,40 +2,71 @@
 //
 // Why pay a fortune teller when you can just program your fortune yourself?
 //
-// // Store the following into variables: number of children,
-// partner's name, geographic location, job title.
-// Output your fortune to the screen like so:
-// "You will be a X in Y, and married to Z with N kids."
-
+// Write a function named tellFortune that:
 //
+// Takes 4 arguments: number of children, partner's name, geographic location, job title.
+// Outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+// Call that function 3 times with 3 different values for the arguments.
 // console.log("JS is working");
 
-var lifeGoals = function(children, partner, geoLocation, job) {
-  return "You will be a " + job + " in " + geoLocation + " , and married to " + partner + " with " + children + " kids.";
+var tellFortune = function(children, partner, geoLocation, job) {
+  console.log( "You will be a " + job + " in " + geoLocation + " , and married to " + partner + " with " + children + " kids.");
 };
 
-console.log(lifeGoals(10, "Wilfred", "Tokyo", "Lawyer") );
+tellFortune(3, "Wilfred", "Dubai", "Window Cleaner");
+tellFortune(0, "Barnabas", "The Artic", "Golfer");
+tellFortune(3, "Homer", "Springfield", "Nuclear plant worker");
+
 
 
 
 // The Age Calculator
 //
-// Forgot how old someone is? Calculate it!
+// Forgot how old you are? Calculate it!
 //
-// Store the current year in a variable.
-// Store their birth year in a variable.
-// Calculate their 2 possible ages based on the stored values.
-// Output them to the screen like so: "They are either NN or NN", substituting the values.
+// Write a function named calculateAge that:
+//
+// Takes 2 arguments: birth year, current year.
+// Calculates the 2 possible ages based on those years.
+// Outputs the result to the screen like so: "You are either NN or NN"
+// Call the function three times with different sets of values.
+// Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
 
-var age = function (birthYear, currentYear) {
-  var possibleAge = currentYear - birthYear;
+
+var calculateAge = function (birthYear) {
+  var possibleAge = (new Date().getFullYear()) - birthYear;
   var possibleAgeTwo = possibleAge - 1;
   console.log("They are either " + possibleAge + " or " + possibleAgeTwo);
   return possibleAge, possibleAgeTwo;
 };
 
+calculateAge(1990);
+calculateAge(1978);
+calculateAge(1939);
 
-age(1939, 2017);
+// The Lifetime Supply Calculator
+//
+// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+//
+// Write a function named calculateSupply that:
+//
+// Takes 2 arguments: age, amount per day.
+// Calculates the amount consumed for rest of the life (based on a constant max age).
+// Outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
+// Call that function three times, passing in different values each time.
+// Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+
+var maxAge = 80;
+var calculateSupply = function (age, amountPerDay) {
+  var snackTotal = (maxAge - age) * amountPerDay;
+  console.log("You will need " + snackTotal + " twix to last you until the ripe old age of " + maxAge);
+  return snackTotal;
+};
+
+calculateSupply(50, 2);
+calculateSupply(23, 5);
+calculateSupply(75, 10);
 
 // The Geometrizer
 //
@@ -70,18 +101,17 @@ calcArea( 5 );
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
-var convertCelc = function ( tempCelc ) {
+var tempCelc = 35;
+var celsiusToFahrenheit = function ( tempCelc ) {
   var convertFarhenheit = ( tempCelc * 9 ) / 5 + 32;
   console.log( tempCelc + "C" + " is " + convertFarhenheit + "F" );
   return convertFarhenheit;
 }
 
-convertCelc(40);
 
-var convertFar = function ( tempFar ) {
+var tempFar = 120;
+var fahrenheitoCelsius = function ( tempFar ) {
   var convertCelcius = ( tempFar -32 ) * 5 / 9;
   console.log( tempFar + "F" + " is " + convertCelcius + "C");
   return convertCelcius;
 }
-
-convertFar(120);
