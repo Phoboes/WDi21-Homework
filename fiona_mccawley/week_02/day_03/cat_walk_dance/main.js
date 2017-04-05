de102a0965101fed339c878a4f609d098686f7ff
fimac <fimccawley@gmail.com>
2017-04-05 22:32:00 +1000

    updated cat dancing with bowling ball

--- fiona_mccawley/week_02/day_03/cat_walk_dance/main.js
+++ fiona_mccawley/week_02/day_03/cat_walk_dance/main.js
@@ -7,10 +7,11 @@ var dancingCat = document.querySelector(".dancing");
 dancingCat.style.display = "none";
 // bowling ball start point
 var bowlingBall = document.querySelector(".ball");
-bowlingBall.style.width = "300px";
-bowlingBall.style.height = "300px";
-bowlingBall.style.display = "none";
-bowlingBall.style.top = "-300px";
+bowlingBall.style.width = "350px";
+bowlingBall.style.height = "350px";
+bowlingBall.style.top = "-350px";
+// var wallpaper = document.querySelector(".wallpaper");
+// var backgroundImg = "url(https://media.giphy.com/media/7CieDdSRoDnFe/giphy.gif)";
 //function for cat to walk out halfway
 var catWalk = function () {
   var width = window.innerWidth - 296; //created variable to store width of screen minus size of cat
@@ -21,18 +22,26 @@ var catWalk = function () {
     window.clearInterval(stopWalk); //stop the interval from running
     image.style.display = "none"; //make the cat disappear
     //dancing cat appears
+    // wallpaper.style.backgroundImage = backgroundImg;
     dancingCat.style.display = "inline"; //make dancing cat appear
     dancingCat.style.bottom = "0px"; //at the bottom
     dancingCat.style.left = (width / 2) + "px"; //halfway across the screen
+
     // I want to remove dancing cat, when the bowling ball falls on top.
     var removeDancingCat = function (){
       dancingCat.style.display = "none";
     };
 window.setTimeout(removeDancingCat, 6000);
-
+window.setTimeout (ballFall, 6000);
     var ballFall = function () {
+
       var ballTop = parseInt(bowlingBall.style.top);
+      var ballBottom = ballTop + 10;
+
+      bowlingBall.style.top = ballBottom + "px";
     };
+
+window.setInterval (ballFall, 20);
   }
 };
 var stopWalk = window.setInterval (catWalk, 50);
