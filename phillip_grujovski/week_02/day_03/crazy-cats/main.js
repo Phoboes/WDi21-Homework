5f674a09d2de74660f9b65d070db2e527092f86f
Philg112 <phil.grujovski@gmail.com>
2017-04-06 22:00:08 +1000

    CRAZY CATS

--- phillip_grujovski/week_02/day_03/crazy-cats/main.js
+++ phillip_grujovski/week_02/day_03/crazy-cats/main.js
@@ -1,103 +1,89 @@
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
-
-// var cats = {
-//     flashingCat: document.querySelectorAll("img")[0],
-//     staticCat: document.querySelectorAll("img")[1],
-    
-//     // FLASHING CAT
-//     flashingCatSetup: {
-//         setFlashingCatStartPosition: function() {
-//             var cat = cats.flashingCat;
-//             cat.style.position = "absolute";
-//             cat.style.right = "0px";
-//             cat.style.left = "0px";
-//             cat.style.top = "40%";
-//         },
-//         walkFlashingCatForward: function() {
-//             var currentLeft = parseInt(cats.flashingCat.style.left);
-//             if(currentLeft < windowWidth) {           
-//                 var desiredLeft = currentLeft + 150;
-//                 cats.flashingCat.style.left = desiredLeft + "px";
-//             }
-//             else{
-//                 clearInterval(f);
-//                 window.setInterval(cats.flashingCatSetup.walkFlashingCatBackward, 100);
-//             }
-//         },
-//         walkFlashingCatBackward: function() {    
-//             if(currentLeft > windowWidth) {
-//                 var currentLeft = parseInt(cats.flashingCat.style.left);
-//                 var desiredLeft = currentLeft - 50;
-//                 cats.flashingCat.style.left = desiredLeft + "px"; 
-//             } else {
-//                 this.fw;
-//                 clearInterval(this.fb);
-//                 return
-//             }
-//         }
-//     },
-
-//     // STATIC CAT
-//     staticCatSetup: {
-//         setStaticCatStartPosition: function() {
-//             var cat = cats.staticCat;
-//             cat.style.position = "absolute";
-//             cat.style.right = "0px";
-//             cat.style.top = "40%";
-//         },
-//         walkStaticCatForward: function() {
-//             var currentRight = parseInt(cats.staticCat.style.right);
-//             var desiredRight = currentRight + 10;
-//             cats.staticCat.style.right = desiredRight + "px";
-//         },
-//         walkStaticCatBackwards: function() {
-            
-//         }
-//     },
-// };
+var flashingDelta = 10;
+var staticDelta = 10;
+var scaredDelta = 10;
+var dogDelta = 10;
 
 
-// cats.flashingCatSetup.setFlashingCatStartPosition();
-// var f = window.setInterval(cats.flashingCatSetup.walkFlashingCatForward, 100);
+var flashingCat = document.querySelectorAll("img")[0];
+flashingCat.style.position = "absolute";
+flashingCat.style.left = "0px";
+flashingCat.style.top = "40%";
+
+var staticCat = document.querySelectorAll("img")[1];
+staticCat.style.position = "absolute";
+staticCat.style.right = "0px";
+staticCat.style.top = "40%";
+
+var scaredCat = document.querySelectorAll("img")[2];
+scaredCat.style.position = "absolute";
+scaredCat.style.right = "0px";
+scaredCat.style.bottom = "0px";
+scaredCat.style.width = "200px";
+
+var breadDog = document.querySelectorAll("img")[3];
+breadDog.style.position = "absolute";
+breadDog.style.right = "0px";
+breadDog.style.top = "0px";
+
+var trumpo = document.querySelectorAll("img")[4];
+trumpo.style.position = "absolute";
+trumpo.style.left = "10%"
+trumpo.style.top = "10%";
+trumpo.style.width = "1200px";
+trumpo.style.zIndex = "-1";
+trumpo.style.visibility = "visible";
 
 
-
-var body = document.body;
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-var walkRight;
-var walkLeft;
-
-var walkingCat = document.querySelector("img");
-walkingCat.style.position = "absolute";
-walkingCat.style.left = "0px";
-walkingCat.style.top = "40%";
-
-function walkCatRight() {
-    var currentLeft = parseInt(walkingCat.style.left);
-    var desiredLeft = currentLeft + 200;
-    walkingCat.style.left = desiredLeft + "px";
-    if(currentLeft > windowWidth) {
-        clearInterval(walkRight);
-        walkLeft = setInterval(walkCatLeft, 500);
+function walkFlashingCat() {
+    var currentLeft = parseInt(flashingCat.style.left);
+    var desiredLeft = currentLeft + flashingDelta;
+    if(desiredLeft < windowWidth - 230) {
+        if(desiredLeft < 0 ) {
+            flashingDelta = 10;
+        }
+        flashingCat.style.left = desiredLeft + "px";
+    } else {
+        flashingDelta = -10;
     }
 }
 
-function walkCatLeft() {
-    var currentLeft = parseInt(walkingCat.style.left);
-    var desiredLeft = currentLeft - 200;
-    walkingCat.style.left = desiredLeft + "px";
-    if(currentLeft < 0) {
-        clearInterval(walkLeft);
-        walkRight = setInterval(walkRight, 500);
+function walkStaticCat() {
+    var currentRight = parseInt(staticCat.style.right);
+    var desiredRight = currentRight +  staticDelta;
+    if(desiredRight < windowWidth - 230 ) {
+        if(desiredRight < 0) {
+            staticDelta = 10;
+        }
+        staticCat.style.right = desiredRight + "px";
+    } else {
+        staticDelta = -10;
     }
 }
 
-function setIntervalAndExecute(func, t) {
-    func();
-    return(setInterval(func, t));
+function animateScaredCat() {
+    var currentWidth = parseInt(scaredCat.style.width);
+    var desiredWidth = currentWidth + scaredDelta;
+    if(desiredWidth < 800) {
+        if(desiredWidth === 0){
+            scaredDelta = 10;
+        }
+        scaredCat.style.width = desiredWidth + "px";
+    } else {
+        scaredDelta = -10;
+    }
 }
 
+function flashTrump() {
+    if(trumpo.style.visibility === "hidden") {
+        trumpo.style.visibility = "visible";
+    } else {
+        trumpo.style.visibility = "hidden";
+    }
+}
 
-walkRight = setInterval(walkCatRight, 500);
+setInterval(flashTrump, 100);
+setInterval(animateScaredCat, 1);
+setInterval(walkStaticCat, 1);
+setInterval(walkFlashingCat, 1);
