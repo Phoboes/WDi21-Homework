950f21a34c13756778c6413efb3fcb5602d47f07
katiepetersonn <katie.peterson19@gmail.com>
2017-04-11 08:34:17 +1000

    all if/else statements working

--- katie_peterson/week_03/tic-tac-toe/main.js
+++ katie_peterson/week_03/tic-tac-toe/main.js
@@ -21,8 +21,8 @@ $(document).ready (function () {
   //   else make it X
   //  Use a variable to detemine the current player
 
-//var playerOne = X;
-//var playerTwo = 0;
+//var playerOne = O;
+//var playerTwo = X;
 
 var player = 1;
   $('.box').click(function () {
@@ -37,20 +37,24 @@ var player = 1;
 
   });
 
+
+  //first row win
+
+
+    //if in row one all boxes equal X
+      //then playerOne wins
+    //Else if all rows equal 0
+      //then playerTwo wins
+
+
   $("body").click(function () {
-    if ($("#box1").text() === "O" && $("#box2").text() === "O") {
-      console.log("Box 1 and box 2 are equal");
-  }
-  });
+    if ($("#box1").text() === "O" && $("#box2").text() === "O" && $("#box3").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+});
 
-
-//first row win
-
-
-  //if in row one all boxes equal X
-    //then playerOne wins
-  //Else if all rows equal 0
-    //then playerTwo wins
 
 //second row win
 
@@ -59,12 +63,28 @@ var player = 1;
 //Else if all rows equal 0
   //then playerTwo wins
 
+  $("body").click(function () {
+    if ($("#box4").text() === "O" && $("#box5").text() === "O" && $("#box6").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box4").text() === "X" && $("#box5").text() === "X" && $("#box6").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
+
 //third row win
 
 //if in the third row all boxes equal X
   //then playerOne wins
 //Else if all rows equal 0
   //then playerTwo wins
+
+  $("body").click(function () {
+    if ($("#box7").text() === "O" && $("#box8").text() === "O" && $("#box9").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box7").text() === "X" && $("#box8").text() === "X" && $("#box9").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
 
 //first column win
 
@@ -73,12 +93,29 @@ var player = 1;
 //Else if all columns equal 0
   //then playerTwo wins
 
+
+  $("body").click(function () {
+    if ($("#box1").text() === "O" && $("#box4").text() === "O" && $("#box7").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box1").text() === "X" && $("#box4").text() === "X" && $("#box7").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
+
 //second column win
 
 //if in the second column all boxes equal X
   //then playerOne wins
 //Else if all columns equal 0
   //then playerTwo wins
+
+  $("body").click(function () {
+    if ($("#box2").text() === "O" && $("#box5").text() === "O" && $("#box8").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box2").text() === "X" && $("#box5").text() === "X" && $("#box8").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
 
 //third column win
 
@@ -87,6 +124,14 @@ var player = 1;
 //Else if all columns equal 0
   //then playerTwo wins
 
+  $("body").click(function () {
+    if ($("#box3").text() === "O" && $("#box6").text() === "O" && $("#box9").text() === "O") {
+      console.log("Player 1 wins!");
+  }  else if ($("#box3").text() === "X" && $("#box6").text() === "X" && $("#box9").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
+
 //right diagonal win
 
 //if in the right diagonal all boxes equal X
@@ -94,12 +139,29 @@ var player = 1;
 //Else if in the right diagonal equal 0
   //then playerTwo wins
 
+  $("body").click(function () {
+    if ($("#box3").text() === "O" && $("#box5").text() === "O" && $("#box7").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box3").text() === "X" && $("#box5").text() === "X" && $("#box7").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
+
+
 //left diagonal win
 
 //if in the left diagonal all boxes equal X
   //then playerOne wins
 //Else if in the left diagonal equal 0
   //then playerTwo wins
+
+  $("body").click(function () {
+    if ($("#box1").text() === "O" && $("#box5").text() === "O" && $("#box9").text() === "O") {
+      console.log("Player 1 wins!");
+  } else if ($("#box1").text() === "X" && $("#box5").text() === "X" && $("#box9").text() === "X") {
+    console.log("Player 2 wins!");
+  };
+  });
 
 
 
