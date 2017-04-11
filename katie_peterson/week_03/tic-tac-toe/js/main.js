1bac2571d4f90d8ae20fa3928d5194421bb328e1
katiepetersonn <katie.peterson19@gmail.com>
2017-04-11 14:54:17 +1000

    animations and sweet alert added

--- katie_peterson/week_03/tic-tac-toe/js/main.js
+++ katie_peterson/week_03/tic-tac-toe/js/main.js
@@ -13,109 +13,104 @@
 //DISPLAY WINNER
 
 //SEPERATE GAME LOGIC FROM PRESENTATION
-
-$(document).ready (function () {
-  // var checkForWin = function () {
-  //   var playerOneScore;
-  //   var playerTwoScore;
-  //
-  //   var playerOneWins = function () {
-  //     $("#message").css("visibility", "visible").html("<p>Player One Wins!</p>");
-  //     playerOneScore = playerOneScore +1;
-  //     $("#player-one .score").html(playerOneScore);
-  //   };
-  //   var playerTwoWins = function () {
-  //   $("#messgae").css("visibility", "visible").html("<p>Player Two Wins!</p>");
-  //   playerTwoScore = playerTwoScore + 1;
-  //   $("#player-two .score").html(playerTwoScore);
-  //   };
-  // };
-
-
-
-  //SWITCH PLAYERS
-  //   if x then make it 0
-  //   else make it X
-  //  Use a variable to detemine the current player
-
 //var playerOne = O;
 //var playerTwo = X;
 
+$(document).ready (function () {
+
 var player = 1;
   $('.box').click(function () {
-    console.log("Clicked!");
-   if(player === 1){
-     $(this).text("O");
+    $(this).addClass("animated flipInX");
+      console.log(this);
+    if(player === 1){
+      $(this).text("O");
      player = 2;
-   } else {
+    } else {
      $(this).text("X");
      player = 1;
    }
-
   });
+
+var playerOneWins = function () {
+  window.setTimeout( function () {
+      swal({
+          title: "Player One Wins",
+          text: "You are a legend!",
+          closeOnConfirm: true,
+          timer: 5000
+      })}, 1000);
+};
+
+
+var playerTwoWins = function () {
+  window.setTimeout( function () {
+      swal({
+        title: "Player Two Wins",
+        text: "You are a superstar!",
+        closeOnConfirm: true,
+        timer: 5000
+    })}, 1000);
+};
+
 
 
   //first row win
-
-
     //if in row one all boxes equal X
       //then playerOne wins
     //Else if all rows equal 0
       //then playerTwo wins
 
-
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box1").text() === "O" && $("#box2").text() === "O" && $("#box3").text() === "O") {
-      alert ("Player 1 wins!");
-  } else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
-    alert ("Player 2 wins!");
+      playerOneWins();
+    } //{
+      // setTimeout(function () {swal("Player 1 wins", "Awesome work!"); }, 1000); }
+    else if ($("#box1").text() === "X" && $("#box2").text() === "X" && $("#box3").text() === "X") {
+      playerTwoWins();
   };
 });
 
 
 //second row win
-
 //if in the second row all boxes equal X
   //then playerOne wins
 //Else if all rows equal 0
   //then playerTwo wins
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box4").text() === "O" && $("#box5").text() === "O" && $("#box6").text() === "O") {
-      alert ("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box4").text() === "X" && $("#box5").text() === "X" && $("#box6").text() === "X") {
-    alert ("Player 2 wins!");
+    playerTwoWins();
   };
   });
 
 //third row win
+  //if in the third row all boxes equal X
+    //then playerOne wins
+  //Else if all rows equal 0
+    //then playerTwo wins
 
-//if in the third row all boxes equal X
-  //then playerOne wins
-//Else if all rows equal 0
-  //then playerTwo wins
-
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box7").text() === "O" && $("#box8").text() === "O" && $("#box9").text() === "O") {
-      alert ("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box7").text() === "X" && $("#box8").text() === "X" && $("#box9").text() === "X") {
-    alert ("Player 2 wins!");
+    playerTwoWins();
   };
   });
 
 //first column win
-
-//if in the first column all boxes equal X
-  //then playerOne wins
-//Else if all columns equal 0
+  //if in the first column all boxes equal X
+    //then playerOne wins
+  //Else if all columns equal 0
   //then playerTwo wins
 
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box1").text() === "O" && $("#box4").text() === "O" && $("#box7").text() === "O") {
-      alert("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box1").text() === "X" && $("#box4").text() === "X" && $("#box7").text() === "X") {
-    alert("Player 2 wins!");
+    playerTwoWins();
   };
   });
 
@@ -126,11 +121,11 @@ var player = 1;
 //Else if all columns equal 0
   //then playerTwo wins
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box2").text() === "O" && $("#box5").text() === "O" && $("#box8").text() === "O") {
-      alert("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box2").text() === "X" && $("#box5").text() === "X" && $("#box8").text() === "X") {
-    alert("Player 2 wins!");
+      playerTwoWins();
   };
   });
 
@@ -141,11 +136,11 @@ var player = 1;
 //Else if all columns equal 0
   //then playerTwo wins
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box3").text() === "O" && $("#box6").text() === "O" && $("#box9").text() === "O") {
-      alert("Player 1 wins!");
+      playerOneWins();
   }  else if ($("#box3").text() === "X" && $("#box6").text() === "X" && $("#box9").text() === "X") {
-    alert("Player 2 wins!");
+      playerTwoWins();
   };
   });
 
@@ -156,11 +151,11 @@ var player = 1;
 //Else if in the right diagonal equal 0
   //then playerTwo wins
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box3").text() === "O" && $("#box5").text() === "O" && $("#box7").text() === "O") {
-      alert("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box3").text() === "X" && $("#box5").text() === "X" && $("#box7").text() === "X") {
-    alert("Player 2 wins!");
+    playerTwoWins();
   };
   });
 
@@ -172,15 +167,13 @@ var player = 1;
 //Else if in the left diagonal equal 0
   //then playerTwo wins
 
-  $("body").click(function () {
+  $(".box").click(function () {
     if ($("#box1").text() === "O" && $("#box5").text() === "O" && $("#box9").text() === "O") {
-      alert("Player 1 wins!");
+      playerOneWins();
   } else if ($("#box1").text() === "X" && $("#box5").text() === "X" && $("#box9").text() === "X") {
-    alert("Player 2 wins!");
+      playerTwoWins();
   };
   });
-
-
 
 //playerOne wins if above conditions are met
 
@@ -189,8 +182,7 @@ var player = 1;
 //reset board
 
 $(".reset").on("click", function ()  {
-    $("td").empty().removeClass("complete xValue oValue");
-    $("#message").css("visibility", "hidden");
+    $("td").empty().removeClass("complete");
 });
 
  }); //doc.ready end of
