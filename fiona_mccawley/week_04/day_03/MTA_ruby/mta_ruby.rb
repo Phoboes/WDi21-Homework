bf50c4511d5819d33e13a3cb5c6541c16ea55cff
fimac <fimccawley@gmail.com>
2017-04-19 22:41:09 +1000

    updated same line journey

--- fiona_mccawley/week_04/day_03/MTA_ruby/mta_ruby.rb
+++ fiona_mccawley/week_04/day_03/MTA_ruby/mta_ruby.rb
@@ -1,4 +1,4 @@
-require "pry"
+# require "pry"
 
 # The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
 # The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
@@ -13,158 +13,109 @@ $line = {
   :L => ["8th L", "6th", "Union Square", "3rd", "1st"],
   "6" => ["Grand Central", "33rd", "28th 6", "23rd 6", "Union Square", "Astor Place"]
 }
-$stop_array = []
-$stop_array_two = []
+$stop_array = [] #array to capture first part of 2 line journey
+$stop_array_two = [] #array to capture second part of 2 line journey
+$stop_array_three = [] #array to capture same line journey
+
+
 
 #first part - forwards to union square
-def trip_one_fwd(line_one, first_stop)
-  # if $line[line_one].index(first_stop) < $line[line_one].index("Union Square")
-
-      i = $line[line_one].index(first_stop)
-      while i <= $line[line_one].index("Union Square")
+def trip_one_fwd( line_one, first_stop )
+      i = $line[line_one].index( first_stop ) + 1
+      while i <= $line[line_one].index( "Union Square" )
         $stop_array.push($line[line_one][i])
         i += 1
       end
 end
 #first part - backwards to union Square
-def trip_one_bwd(line_one, first_stop)
-  # if $line[line_one].index(first_stop) > $line[line_one].index("Union Square")
-
-    i = $line[line_one].index(first_stop)
-    while i >= $line[line_one].index("Union Square")
+def trip_one_bwd( line_one, first_stop )
+    i = $line[line_one].index( first_stop ) - 1
+    while i >= $line[line_one].index( "Union Square" )
       $stop_array.push($line[line_one][i])
       i -= 1
     end
 end
 #second part - forwards from union Square
-def trip_two_fwd
-  if $line[line_two].index("Union Square") < $line[line_two].index(second_stop)
-
-      i = $line[line_two].index("Union Square")
-      while i <= $line[line_two].index(second_stop)
+def trip_two_fwd( line_two, second_stop )
+      i = $line[line_two].index( "Union Square" ) + 1
+      while i <= $line[line_two].index( second_stop )
         $stop_array_two.push($line[line_two][i])
         i += 1
       end
-    end
 end
 #second part - backwards from union square
-def trip_two_bwd
-  if $line[line_two].index("Union Square") > $line[line_two].index(second_stop)
-
-    i = $line[line_two].index("Union Square")
-    while i >= $line[line_two].index(second_stop)
+def trip_two_bwd( line_two, second_stop )
+    i = $line[line_two].index("Union Square") - 1
+    while i >= $line[line_two].index( second_stop )
       $stop_array_two.push($line[line_two][i])
       i -= 1
     end
-  end
 end
 
 
-
-
-def journey(line_one, first_stop, line_two, second_stop)
+def plan_trip( line_one, first_stop, line_two, second_stop )
 
 # if different lines
 if $line[line_one] != $line[line_two]
-
-    if $line[line_one].index(first_stop) < $line[line_one].index("Union Square")
-      trip_one_fwd(line_one, first_stop)
+    #first part if fwds
+    if $line[line_one].index( first_stop ) < $line[line_one].index( "Union Square" )
+      trip_one_fwd( line_one, first_stop )
     end
-
-    if $line[line_one].index(first_stop) > $line[line_one].index("Union Square")
+    #first part if bwds
+    if $line[line_one].index( first_stop ) > $line[line_one].index( "Union Square" )
       trip_one_bwd(line_one, first_stop)
     end
-end
+    #second part if fwds
+    if $line[line_two].index( "Union Square" ) < $line[line_two].index( second_stop )
+      trip_two_fwd(line_two, second_stop)
+    end
+    #second part if bwds
+    if $line[line_two].index( "Union Square" ) > $line[line_two].index( second_stop )
+      trip_two_bwd(line_two, second_stop)
+    end
 
+    stops = $stop_array.join(", " )
+    stops_two = $stop_array_two.join(", ")
+    total_stops = $stop_array.length + $stop_array_two.length
 
-#
-# # forwards on same line
-#   if $line[line_one].index(first_stop) < $line[line_one].index(second_stop)
-#
-#       i = $line[line_one].index(first_stop)
-#       while i <= $line[line_one].index(second_stop)
-#         $stop_array.push($line[line_one][i])
-#         i += 1
-#       end
-#     end
-# # backwards on same line
-#    if $line[line_one].index(first_stop) > $line[line_one].index(second_stop)
-#
-#      i = $line[line_one].index(first_stop)
-#      while i >= $line[line_one].index(second_stop)
-#        $stop_array.push($line[line_one][i])
-#        i -= 1
-#      end
-#    end
-  #  number_of_stops = $stop_array.length
-   stops = $stop_array.join(", " )
-   stops_two = $stop_array_two.join(", ")
-
-  p "Take the #{line_one} line, you will stop at the following stations: #{stops}. Change at Union Square, and get onto the #{line_two} line, and go through the follow stations #{stops_two}"
+   p "Take the #{line_one} line, you will stop at the following stations: #{stops}."
+   p "Change at Union Square"
+   p "Get onto the #{line_two} line, and go through the follow stations #{stops_two}"
+   p "Total stops: #{total_stops}"
 
 end
-journey(:L, "1st", :N, "8th N")
 
+#if the same line
+if $line[line_one] == $line[line_two]
+# forwards on same line
+  if $line[line_one].index( first_stop ) < $line[line_one].index( second_stop )
 
+      i = $line[line_one].index( first_stop ) + 1
+      while i <= $line[line_one].index( second_stop )
+        $stop_array_three.push($line[line_one][i])
+        i += 1
+      end
 
+    end
+# backwards on same line
+   if $line[line_one].index( first_stop ) > $line[line_one].index( second_stop )
 
+     i = $line[line_one].index( first_stop ) - 1
+     while i >= $line[line_one].index( second_stop )
+       $stop_array_three.push($line[line_one][i])
+       i -= 1
+     end
 
+   end
 
+   stops_three = $stop_array_three.join(", ")
+   total_stops = $stop_array_three.length
 
-
-
-
-
-
-
-
-
-
-#
-#
-#
-# // var travel = function( line, startStation, endingStation ){
-# //   var returnArray = [];
-#
-# # //   // If the start looping through from the starting index, to the end.
-# # //   // Store all stops along the way in an array.
-#
-# //   for( var i = line.indexOf( startStation ); i <= line.indexOf( endingStation ); i ++ ){
-# //     returnArray.push( line[i] );
-# //   }
-# //   return returnArray;
-# // };
-#
-#
-# var travel = function( line, startStation, endingStation ){
-#   var returnArray = [];
-#   #
-#   # // We know that the single line works now. Rather than re-use code, if we have an array where we know our start index is higher than our end (further down the array), just reverse the array.
-#
-#   if( line.indexOf( endingStation ) < line.indexOf( startStation ) ){
-#     line = line.reverse();
-#   }
-#
-#
-#   for( var i = line.indexOf( startStation ); i <= line.indexOf( endingStation ); i ++ ){
-#     returnArray.push( line[i] );
-#   }
-#   return returnArray;
-# };
-#
-# var makeTrip = function( startingLine, startStation, endingLine, endingStation ){
-#
-#   if( startingLine !== endingLine ){
-#
-#     // We know we will be getting 2 arrays ending or starting at Union Square (the common point between our lines), we can use concat to join them.
-#     return travel( startingLine, startStation, "Union Square" )
-#           .concat( travel( endingLine, "Union Square", endingStation ) );
-#   }
-#
-#   return travel( startingLine, startStation, endingStation );
-#
-# };
-
+   p "Take the #{line_one}, line you will go through the following stations #{stops_three}."
+   p "Total stops: #{total_stops}"
+ end
+end
+plan_trip("6", "Grand Central", :L, "8th L")
 
 
 
