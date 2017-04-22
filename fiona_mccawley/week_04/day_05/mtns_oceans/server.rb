a95bf75b8f444122d3d69475efa16af66ff3e2f0
fimac <fimccawley@gmail.com>
2017-04-22 14:33:04 +1000

    band index/list created

--- fiona_mccawley/week_04/day_05/mtns_oceans/server.rb
+++ fiona_mccawley/week_04/day_05/mtns_oceans/server.rb
@@ -3,10 +3,30 @@ require "sinatra"
 require "sinatra/reloader"
 require "sqlite3"
 
+# READ STEP - /bands
+# CREATE STEP - /bands/new
+# UPDATE STEP - /bands/:id/edit
+# DELETE STEP - /bands/:id/delete
+
 get "/" do
   erb(:home)
 end
 
 get "/about" do
   erb(:about)
+end
+
+get "/bands" do
+  db = SQLite3::Database.new("database.db")
+  db.results_as_hash = true
+
+  @all_bands = db.execute "SELECT * FROM bands"
+
+  db.close
+
+  erb(:band_index)
+end
+
+get "/bands/:id" do
+  "This is a bands page"
 end
