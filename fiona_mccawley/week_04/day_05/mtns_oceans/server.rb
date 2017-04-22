c5c562c274933610d707f055da67c18029354285
fimac <fimccawley@gmail.com>
2017-04-22 16:54:01 +1000

    added engineer pages for index, new, edit. Delete functions

--- fiona_mccawley/week_04/day_05/mtns_oceans/server.rb
+++ fiona_mccawley/week_04/day_05/mtns_oceans/server.rb
@@ -16,6 +16,10 @@ get "/about" do
   erb(:about)
 end
 
+# ------------------------------------------------
+# ENGINEERS
+# ------------------------------------------------
+
 get "/bands" do
   db = SQLite3::Database.new("database.db")
   db.results_as_hash = true
@@ -100,10 +104,89 @@ get "/bands/:id/delete" do
   redirect("/bands")
 end
 
+# ------------------------------------------------
+# ENGINEERS
+# ------------------------------------------------
+
 get "/engineers" do
   db = SQLite3::Database.new("database.db")
   db.results_as_hash = true
   @all_engineers = db.execute "SELECT * FROM engineers"
   db.close
   erb(:engineer_index)
+end
+
+# input
+get "/engineer/new" do
+  erb(:engineer_new)
+end
+
+# submit part
+post "/engineers" do
+
+  name = params["name"]
+  bio = params["bio"]
+  gear = params["gear"]
+  img = params["image"]
+
+  sql = "INSERT INTO engineers (name, bio, gear, image) VALUES ('#{name}', '#{bio}', '#{gear}', '#{img}')"
+  db = SQLite3::Database.new("database.db")
+  db.results_as_hash = true
+  db.execute( sql )
+  db.close
+
+  redirect("/engineers")
+end
+
+get "/engineers/:id" do
+  id = params["id"]
+
+  db = SQLite3::Database.new("database.db")
+  db.results_as_hash = true
+  sql = "SELECT * FROM engineers WHERE id == #{id}"
+  @engineer = db.execute(sql).first
+  db.close
+
+  erb(:engineer_show)
+end
+
+get "/engineers/:id/edit" do
+
+  id = params["id"]
+
+  db = SQLite3::Database.new("database.db")
+  db.results_as_hash = true
+  sql = "SELECT * FROM engineers WHERE id == #{id}"
+  @engineer = db.execute(sql).first
+  db.close
+
+  erb(:engineers_edit)
+end
+
+post "/engineers/:id" do
+
+  id = params["id"]
+  name = params["name"]
+  bio = params["bio"]
+  gear = params["gear"]
+  image = params["image"]
+
+  sql = "UPDATE engineers SET name = '#{name}', bio = '#{bio}', gear = '#{gear}', image = '#{image}' WHERE id == #{id}"
+
+  db = SQLite3::Database.new("database.db")
+  db.results_as_hash = true
+  db.execute(sql)
+  db.close
+
+  redirect("/engineers/#{id}")
+
+end
+
+get "/engineers/:id/delete" do
+  id = params["id"]
+  db = SQLite3::Database.new("database.db")
+  sql = "DELETE FROM engineers WHERE id == #{id}"
+  db.execute(sql)
+  db.close
+  redirect("/engineers")
 end
