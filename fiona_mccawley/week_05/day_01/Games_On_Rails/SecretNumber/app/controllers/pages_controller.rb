81ded3ad45a45e48f82ed176cdb138aaffe66105
fimac <fimccawley@gmail.com>
2017-04-24 17:33:57 +1000

    basic magic 8 ball game via url q added

--- fiona_mccawley/week_05/day_01/Games_On_Rails/SecretNumber/app/controllers/pages_controller.rb
+++ fiona_mccawley/week_05/day_01/Games_On_Rails/SecretNumber/app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
 
   end
 
+  def secret
+
+  end
+
   def secret_number
   # Secret Number
   #
@@ -20,5 +24,39 @@ class PagesController < ApplicationController
     end
 
   end
+
+  def magic
+
+  end
+
+  def magic_response
+      # Magic 8 Ball
+      #
+      # Magic 8 ball takes user's questions from the the URL as params and returns a positive or negative answer.
+      ball_response =
+      ["It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Most likely",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Outlook not so good",
+      "Very doubtful"]
+      @response = ball_response.sample()
+
+  end
+
 
 end
--- fiona_mccawley/week_05/day_01/Games_On_Rails/SecretNumber/config/routes.rb
+++ fiona_mccawley/week_05/day_01/Games_On_Rails/SecretNumber/config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   get "/home" => "pages#home"
 
+  get "/secret" => "pages#secret"
 
   get "/secret/:number" => "pages#secret_number"
+
+  get "/magic" => "pages#magic"
+
+  get "/magic/:question" => "pages#magic_response"
 
 end
