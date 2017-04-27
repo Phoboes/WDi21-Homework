be424d2cc752cb9bb7b88f22b3d2c77f780ab3fc
fimac <fimccawley@gmail.com>
2017-04-27 21:34:05 +1000

    update, delete added

--- fiona_mccawley/week_05/day_03/VenueApp/app/controllers/events_controller.rb
+++ fiona_mccawley/week_05/day_03/VenueApp/app/controllers/events_controller.rb
@@ -8,20 +8,33 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
-
+    event = Event.create ( event_params)
+    redirect_to event_path(event)
   end
 
   def edit
+
+    @event = Event.find_by(id: params["id"])
   end
 
   def update
-
+    event = Event.find_by(id: params["id"])
+    event.update (event_params)
+    redirect_to event_path(event)
   end
 
   def destroy
+    event = Event.find_by(id: params["id"])
+    event.destroy
+    redirect_to events_path
+  end
 
+  private
+  def event_params
+    params.require(:event).permit(:band, :date, :ticket, :photos)
   end
 end
