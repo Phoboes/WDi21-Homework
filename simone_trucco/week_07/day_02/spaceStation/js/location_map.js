dbbc93bfa3a96cccb7408870f1cc53d5b9a658ea
SimoTr84 <simone.trucco@gmail.com>
2017-05-10 09:12:56 +1000

    started working on geolocation on GMaps

--- simone_trucco/week_07/day_02/spaceStation/js/location_map.js
+++ simone_trucco/week_07/day_02/spaceStation/js/location_map.js
@@ -2,6 +2,11 @@ var ISS_URL = "http://api.open-notify.org/iss-now.json";
 var ISS_METHOD = "GET";
 var ISS_DATA_TYPE = "JSONP";
 
+var MAP_URL = "url";
+var MAP_METHOD = "GET";
+var MAP_DATA_TYPE = "JSON;";
+var API_KEY = 023343423;
+
 var displayPosition = function (data) {
 
   var lat = data.iss_position.latitude;
@@ -10,12 +15,12 @@ var displayPosition = function (data) {
   $(".latitude").text( lat );
   $(".longitude").text( lon );
 
-  // var lat = data.iss_position.latitude;
-  // var lon = data.iss_position.longitude;
-  // var currentPosition = lat + lon;
-  // var $newPosition = $("<p></p>");
-  // $newPosition.text(currentPosition);
-  // $newposition.appendTo( ".position" );
+//   var lat = data.iss_position.latitude;
+//   var lon = data.iss_position.longitude;
+//   var currentPosition = lat + lon;
+//   var $newPosition = $("<p></p>");
+//   $newPosition.text(currentPosition);
+//   $newposition.appendTo( ".position" );
 };
 
 var issPosition = function (){
@@ -33,11 +38,23 @@ $(document).ready(function () {
   var ISS_POSITION = window.setInterval(function () {
     issPosition();
   }, 3000);
+});
 
+var issGeolocation = function () {
+    $.ajax ({
+      url: MAP_URL,
+      method: MAP_METHOD,
+      dataType: MAP_DATA_TYPE,
+      data: {
+        api_key: "key",
+        latitude: "lat",
+        longitude: "lon"
+      }
+    });
+};
 
   // HOW DO I APPEND ONE LOCATION AFTER ANOTHER?!?!?!?!
 
   // $(".stop button").on("click", function () {
   //   window.clearInterval( CHUCK_TIMER );
   // });
-});
